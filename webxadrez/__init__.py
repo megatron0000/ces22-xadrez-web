3aeb0baed620c769d645e74c22a3def03ad450e6
Vitor Arruda <vitor_pimenta20@hotmail.com>
2018-05-17 15:12:15 -0300

    Cria apps emailsignup, gmailbox
    Restringe origens de websocket

--- webxadrez/__init__.py
+++ webxadrez/__init__.py
@@ -1,5 +1,5 @@
 from os import environ
 
-file = open(environ.get('GMAIL_CLIENT_SECRET_PATH'))
+file = open(environ.get('GMAIL_CLIENT_SECRET_PATH'), 'w')
 file.write(environ.get('GMAIL_CLIENT_DUMP'))
 file.close()
