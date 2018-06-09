a32a2406833e308454034d3b8dca215d48b8e946
GUSTAVO <oliveiramartinsgustavo@gmail.com>
2018-06-09 14:22:40 -0300

    Implementa GameSession

--- chessgames/models.py
+++ chessgames/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 from chatchannels.models import ChatChannel
 
 
 # Create your models here.
+class ChessGame(models.Model):
+    history = ArrayField(base_field=models.TextField())
+    white = models.ForeignKey(User, related_name="white_games")
+    black = models.ForeignKey(User, related_name="white_games", blank=True)
+    start = models.DateTimeField(auto_now_add=True)
+    end = models.DateTimeField(blank=True)
+    win = models.CharField(max_length=10, blank=10)
+    alive = models.BooleanField(default=False)
 
 class GameSession(models.Model):
     channel = models.ForeignKey(ChatChannel)
