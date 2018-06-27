0dad120d762d8097ab3383c9a36f269ce0bfa5b9
Vitor Arruda <vitor_pimenta20@hotmail.com>
2018-06-27 15:17:15 -0300

    Insere sitetreedump.json para load no servidor
    Possibilita DEBUG=False por ambiente

--- webxadrez/settings.py
+++ webxadrez/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get(
     'SECRET_KEY', 's2@garpy5!f@ye#*!2+0y#ruftsy#hjn(29qkp(4j19*os$iop')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = os.environ.get(
     'ALLOWED_HOSTS', 'localhost').split(':') + ['localhost']
